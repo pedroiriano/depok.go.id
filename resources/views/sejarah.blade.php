f92e4f07f5e348a6477bbe1482d2109392c12696
Bio Abidzar <biobun@gmail.com>
2020-04-24 12:53:14 +0700

    Remove and Fixed Sejarah Depok Page

--- resources/views/sejarah.blade.php
+++ resources/views/sejarah.blade.php
@@ -5,8 +5,6 @@
 <div class="container-fluid">
     <div class="container">
         <h1 class="display-3 py-5">Sejarah</h1>
-        {{-- <div id="quill"></div>
-        <p id="htmlContent"></p> --}}
         <div class="row py-4 text-justify">
             <p class="lead">Depok berawal dari sebuah Kecamatan yang berada di lingkungan Kewedanaan (Pembantu Bupati) wilayah Parung Kabupaten Bogor, kemudian pada tahun 1976 perumahan mulai dibangun baik oleh Perum Perumnas maupun pengembang yang kemudian diikuti dengan dibangunnya kampus Universitas Indonesia (UI), serta meningkatnya perdagangan dan Jasa yang semakin pesat sehingga diperlukan kecepatan pelayanan.</p>
             
@@ -18,42 +16,30 @@
             </ul>
             <p class="lead">Selama kurun waktu 17 tahun Kota Administratif Depok berkembang pesat baik dibidang Pemerintahan, Pembangunan dan Kemasyarakatan. Khususnya bidang Pemerintahan semua Desa berganti menjadi Kelurahan dan adanya pemekaran Kelurahan , sehingga pada akhirnya Depok terdiri dari 3 (Kecamatan) dan 23 (dua puluh tiga) Kelurahan, yaitu :</p>
             <ul class="lead">
-    <li>Kecamatan Pancoran Mas, terdiri dari 6 (enam) Kelurahan, yaitu : Kelurahan Depok, Kelurahan Depok Jaya, Kelurahan Pancoran Mas, Kelurahjn Rangkapan Jaya, Kelurahan Rangkapan Jaya Baru.</li>
-    <li>Kecamatan Beji terdiri dari (enam) Kelurahan, yaitu : Kelurahan Beji, Kelurahan Beji Timur, Kelurah Pondok Cina, Kelurahan Kemirimuka, Kelurahan Kukusan, Kelurahan Tanah Baru.</li>
-    <li>Kecamatan Sukmajaya, terdiri dari 11 (sebelas) Kelurahan, yaitu : Kelurahan Sukmajaya, Kelurahan Suka Maju,. Kelurahan Mekarjaya, Kelurahan Abadi Jaya, Kelurahan Baktijaya, Kelurahan Cisalak, Kelurahan Kalibaru, Kelurahan Kalimulya, Kelurahan Kali Jaya, Kelurahan Cilodong, Kelurahan Jati Mulya, Kelurahan Tirta Jaya.</li>
-</ul>
-<p class="lead">Terbentuknya Kota Depok</p>
+                <li>Kecamatan Pancoran Mas, terdiri dari 6 (enam) Kelurahan, yaitu : Kelurahan Depok, Kelurahan Depok Jaya, Kelurahan Pancoran Mas, Kelurahjn Rangkapan Jaya, Kelurahan Rangkapan Jaya Baru.</li>
+                <li>Kecamatan Beji terdiri dari (enam) Kelurahan, yaitu : Kelurahan Beji, Kelurahan Beji Timur, Kelurah Pondok Cina, Kelurahan Kemirimuka, Kelurahan Kukusan, Kelurahan Tanah Baru.</li>
+                <li>Kecamatan Sukmajaya, terdiri dari 11 (sebelas) Kelurahan, yaitu : Kelurahan Sukmajaya, Kelurahan Suka Maju,. Kelurahan Mekarjaya, Kelurahan Abadi Jaya, Kelurahan Baktijaya, Kelurahan Cisalak, Kelurahan Kalibaru, Kelurahan Kalimulya, Kelurahan Kali Jaya, Kelurahan Cilodong, Kelurahan Jati Mulya, Kelurahan Tirta Jaya.</li>
+            </ul>
+            <p class="lead">Terbentuknya Kota Depok</p>
 
-<p class="lead">Dengan semakin pesatnya perkembangan dan tuntutan aspirasi masyarakat yang semakin mendesak agar Kota Administratif Depok diangkat menjadi Kotamadya dengan harapan pelayanan menjadi maksimum. Disis lain Pemerintah Kabupaten Bogor bersama – sama Pemerintah Propinsi Jawa Barat memperhatikan perkembangan tesebut, dan mengusulkannya kepada Pemerintah Pusat dan Dewan Perwakilan Rakyat. Berdasarkan Undang – undang No. 15 tahun 1999, tentang pembentukan Kotamadya Daerah Tk. II Depok yang ditetapkan pada tanggal 20 April 1999, dan diresmikan tanggal 27 April 1999 berbarengan dengan Pelantikan Pejabat Walikotamadya Kepala Daerah Tk. II Depok yang dipercayakan kepada Drs. H. Badrul Kamal yang pada waktu itu menjabat sebagai Walikota Kota Administratif Depok. Momentum peresmian Kotamadya Daerah Tk. II Depok dan pelantikan pejabat Walikotamadya Kepala Daerah Tk. II Depok dapat dijadikan suatu landasan yang bersejarah dan tepat untuk dijadikan hari jadi Kota Depok.</p>
+            <p class="lead">Dengan semakin pesatnya perkembangan dan tuntutan aspirasi masyarakat yang semakin mendesak agar Kota Administratif Depok diangkat menjadi Kotamadya dengan harapan pelayanan menjadi maksimum. Disis lain Pemerintah Kabupaten Bogor bersama – sama Pemerintah Propinsi Jawa Barat memperhatikan perkembangan tesebut, dan mengusulkannya kepada Pemerintah Pusat dan Dewan Perwakilan Rakyat. Berdasarkan Undang – undang No. 15 tahun 1999, tentang pembentukan Kotamadya Daerah Tk. II Depok yang ditetapkan pada tanggal 20 April 1999, dan diresmikan tanggal 27 April 1999 berbarengan dengan Pelantikan Pejabat Walikotamadya Kepala Daerah Tk. II Depok yang dipercayakan kepada Drs. H. Badrul Kamal yang pada waktu itu menjabat sebagai Walikota Kota Administratif Depok. Momentum peresmian Kotamadya Daerah Tk. II Depok dan pelantikan pejabat Walikotamadya Kepala Daerah Tk. II Depok dapat dijadikan suatu landasan yang bersejarah dan tepat untuk dijadikan hari jadi Kota Depok.</p>
 
-<p class="lead">Berdasarkan Undang – undang nomor 15 tahun 1999 Wilayah Kota Depok meliputi wilayah Administratif Kota Depok, terdiri dari 3 (tiga) Kecamatan sebagaimana tersebut diatas ditambah dengan sebagian wilayah Kabupaten Daerah Tingkat II Bogor, yaitu :</p>
-<ul class="lead">
-   <li>
-    Kecamatan Cimanggis, yang terdiri dari 1 (satu) Kelurahan dan 12 (dua belas) Desa , yaitu : Kelurahan Cilangkap, Desa Pasir Gunung Selatan, Desa Tugu, Desa Mekarsari, Desa Cisalak Pasar, Desa Curug, Desa Hajarmukti, Desa Sukatani, Desa Sukamaju Baru, Desa Cijajar, Desa Cimpaeun, Desa Leuwinanggung.</li>
-    <li>Kecamatan Sawangan, yang terdiri dari 14 (empat belas) Desa, yaitu : Desa Sawangan, Desa Sawangan Baru, Desa Cinangka, Desa Kedaung, Desa Serua, Desa Pondok Petir, Desa Curug, Desa Bojong Sari, Desa Bojong Sari Baru, Desa Duren Seribu, Desa Duren Mekar, Desa Pengasinan Desa Bedahan, Desa Pasir Putih</li>
-    <li>Kecamatan Limo yang terdiri dari 8 (delapan) Desa, yaitu : Desa Limo, Desa Meruyung, Desa Cinere, Desa Gandul, Desa Pangkalan Jati, Desa Pangkalan Jati Baru, Desa Krukut, Desa Grogol.</li>
-    <li>Dan ditambah 5 (lima) Desa dari Kecamatan Bojong Gede, yaitu : Desa Cipayung, Desa Cipayung Jaya, Desa Ratu Jaya, Desa Pondok Terong, Desa Pondok Jaya.</li>
-</ul>
-<p class="lead">Kota Depok selain merupakan Pusat Pemerintahan yang berbatasan langsung dengan Wilayah Daerah Khusus Ibu Kota Jakarta juga merupakan wilayah penyangga Ibu Kota Negara yang diarahkan untuk kota pemukiman , Kota Pendidikan, Pusat pelayanan perdagangan dan jasa, Kota pariwisata dan sebagai kota resapan air.</p>
-             <div class="col-12">
+            <p class="lead">Berdasarkan Undang – undang nomor 15 tahun 1999 Wilayah Kota Depok meliputi wilayah Administratif Kota Depok, terdiri dari 3 (tiga) Kecamatan sebagaimana tersebut diatas ditambah dengan sebagian wilayah Kabupaten Daerah Tingkat II Bogor, yaitu :</p>
+            <ul class="lead">
+               <li>
+                Kecamatan Cimanggis, yang terdiri dari 1 (satu) Kelurahan dan 12 (dua belas) Desa , yaitu : Kelurahan Cilangkap, Desa Pasir Gunung Selatan, Desa Tugu, Desa Mekarsari, Desa Cisalak Pasar, Desa Curug, Desa Hajarmukti, Desa Sukatani, Desa Sukamaju Baru, Desa Cijajar, Desa Cimpaeun, Desa Leuwinanggung.</li>
+                <li>Kecamatan Sawangan, yang terdiri dari 14 (empat belas) Desa, yaitu : Desa Sawangan, Desa Sawangan Baru, Desa Cinangka, Desa Kedaung, Desa Serua, Desa Pondok Petir, Desa Curug, Desa Bojong Sari, Desa Bojong Sari Baru, Desa Duren Seribu, Desa Duren Mekar, Desa Pengasinan Desa Bedahan, Desa Pasir Putih</li>
+                <li>Kecamatan Limo yang terdiri dari 8 (delapan) Desa, yaitu : Desa Limo, Desa Meruyung, Desa Cinere, Desa Gandul, Desa Pangkalan Jati, Desa Pangkalan Jati Baru, Desa Krukut, Desa Grogol.</li>
+                <li>Dan ditambah 5 (lima) Desa dari Kecamatan Bojong Gede, yaitu : Desa Cipayung, Desa Cipayung Jaya, Desa Ratu Jaya, Desa Pondok Terong, Desa Pondok Jaya.</li>
+            </ul>
+            <p class="lead">Kota Depok selain merupakan Pusat Pemerintahan yang berbatasan langsung dengan Wilayah Daerah Khusus Ibu Kota Jakarta juga merupakan wilayah penyangga Ibu Kota Negara yang diarahkan untuk kota pemukiman , Kota Pendidikan, Pusat pelayanan perdagangan dan jasa, Kota pariwisata dan sebagai kota resapan air.</p>
+            <div class="col-12">
                 <h1 class="display-5 py-5">Kepemimpinan 1982 -2019</h1>
             </div>
             <div class="col-12 py-5">
-            <img src="{{ asset('images/pempindepok.svg') }}" alt="" class="img-fluid">
+                <img src="{{ asset('images/pempindepok.svg') }}" alt="" class="img-fluid">
             </div>
         </div>
     </div>
 </div>
-<script>
-var htmlContent = document.getElementById('htmlContent');
-var options = {
-    modules: {
-      toolbar: false,
-    },
-    theme: 'snow',
-};
-var editor = new Quill('#quill', options);
-editor.setContents({!! $sejarah->content !!});
-htmlContent.innerHTML = editor.root.innerHTML;
-</script>
 @endsection
