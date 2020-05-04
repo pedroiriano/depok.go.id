a241ab071581ba0001740ee7d2c0c90d93177f53
Bio Abidzar <biobun@gmail.com>
2020-05-04 14:35:48 +0700

    Add meta description

--- resources/views/includes/layout.blade.php
+++ resources/views/includes/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,minimum-scale=1,maximum-scale=1,initial-scale=1">
-
+    <meta name="description" content="Portal Resmi Pemerintah Kota Depok. Depok sebagai Kota untuk Berbagi, Bersahabat dan Berkolaborasi">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
