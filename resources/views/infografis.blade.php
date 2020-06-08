969a26ecb58bd4076a77b87dbd7c392c6e8684d3
Bio Abidzar <biobun@gmail.com>
2020-06-08 13:55:39 +0700

    Fixed Infografis

--- resources/views/infografis.blade.php
+++ resources/views/infografis.blade.php
@@ -5,44 +5,40 @@
 <div class="container-fluid">
     <div class="container">
         <h1 class="display-3 py-5">Infografis</h1>
-        @foreach ($infografis as $infografi)
         <div class="row py-5">
-            <div class="col-12 col-md-3">
-                <a data-toggle="modal" data-target="#modalInfografis" href="#">
-                <img src="{{ asset('uploads/infografis/'.$infografi->imageName) }}" alt="" class="img-fluid">
-                </a>
+        @foreach ($infografis as $infografi)
+            <div class="col-12 col-md-3 d-flex align-items-stretch">
+                <div class="card mb-3">
+                    <a href="#" data-target="#modalInfografis-{{ $infografi->id }}" data-toggle="modal">
+                        <img class="card-img-top" src="{{ asset('storage/uploads/infografis/'.$infografi->imageName) }}" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $infografi->nama }}</h5>
+                            <p class="card-text"><small class="text-muted">Sumber: {{ $infografi->opd->nama }}</small></p>
+                        </div>
+                    </a>
+                </div>
             </div>
-            <div class="col-md">
-                <p class="lead font-weight-bold">
-                    {{ $infografi->nama }}
-                </p>
-                <p class="lead">
-                    Sumber:
-                    {{ $infografi->sumber }}
-                </p>
-            </div>
-        </div>
-        <!-- Modal Infografis-->
-        <div class="modal fade" id="modalInfografis" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">{{ $infografi->nama }}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <img src="{{ asset('uploads/infografis/'.$infografi->imageName) }}" alt="" class="img-fluid">
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+            <!-- Modal Infografis-->
+            <div class="modal fade" id="modalInfografis-{{ $infografi->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">{{ $infografi->nama }}</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body text-center">
+                            <img src="{{ asset('uploads/infografis/'.$infografi->imageName) }}" alt="" class="img-fluid">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         @endforeach
+        </div>
     </div>
 </div>
 </div>
