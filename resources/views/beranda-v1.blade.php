3b26f8d94bf60b3326aeb88595d2ec24e58dd9d5
Bio Abidzar <biobun@gmail.com>
2022-03-04 10:31:53 +0700

    remove agenda modal in beranda

--- resources/views/beranda-v1.blade.php
+++ resources/views/beranda-v1.blade.php
@@ -249,25 +249,6 @@
                                 </div>
                             </div>
                         </div>
-                        <!-- Modal Agenda-->
-                        <div class="modal fade" id="modalAgenda-{{ $agenda->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-lg" role="document">
-                               <div class="modal-content">
-                                  <div class="modal-header">
-                                     <h5 class="modal-title" id="exampleModalLabel">{{ $agenda->nama }}</h5>
-                                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                     </button>
-                                  </div>
-                                  <div class="modal-body text-center">
-                                     <img src="{{ asset('uploads/agenda/'.$agenda->imageName) }}" alt="" class="img-fluid" loading="lazy">
-                                  </div>
-                                  <div class="modal-footer">
-                                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                  </div>
-                               </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
