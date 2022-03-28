af31b03b20bb9200b30b51cc9c0c535fd73f9db9
biobun <biobun@gmail.com>
2022-03-28 18:51:15 +0700

    Change tinymce

--- webpack.mix.js
+++ webpack.mix.js
@@ -21,15 +21,7 @@ mix.js('resources/js/app.js', 'public/js')
     //         }
     //     }
     // });
+mix.copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce');
 mix.options({
     processCssUrls: false // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
 });
-// mix.browserSync('127.0.0.1:8000');
-
-mix.copyDirectory('node_modules/tinymce/plugins', 'public/node_modules/tinymce/plugins');
-mix.copyDirectory('node_modules/tinymce/skins', 'public/node_modules/tinymce/skins');
-mix.copyDirectory('node_modules/tinymce/themes', 'public/node_modules/tinymce/themes');
-mix.copy('node_modules/tinymce/jquery.tinymce.js', 'public/node_modules/tinymce/jquery.tinymce.js');
-mix.copy('node_modules/tinymce/jquery.tinymce.min.js', 'public/node_modules/tinymce/jquery.tinymce.min.js');
-mix.copy('node_modules/tinymce/tinymce.js', 'public/node_modules/tinymce/tinymce.js');
-mix.copy('node_modules/tinymce/tinymce.min.js', 'public/node_modules/tinymce/tinymce.min.js');
