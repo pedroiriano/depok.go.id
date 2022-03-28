c0d18d38e6fe3d106d0f7d5a574a74e3ea9a10c4
biobun <biobun@gmail.com>
2022-03-28 21:37:21 +0700

    fix json_encode

--- resources/views/admin/content.blade.php
+++ resources/views/admin/content.blade.php
@@ -21,62 +21,30 @@
     <form method="POST" action="{{ route('admin.update.content', $content->slug) }}" enctype="multipart/form-data" id="formSejarah">
         {{ csrf_field() }}
         <div class="form-group">
-            <textarea class="content" name="inputContent"></textarea>
+            <textarea class="content" name="inputContent" id="content"></textarea>
         </div>
-        {{ $content->desc }}
+        {!! $content->desc !!}
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 </div>
+@endsection
 @push('js')
 <script src="{{ asset('js/tinymce/tinymce.min.js') }}" referrerpolicy="origin"></script>
 <script>
-    $('.content').html('{!! $content->desc !!} ');
     tinymce.init({
         selector:'textarea.content',
-        plugins: 'image code',
+        plugins: 'code',
         menubar: false,
         statusbar: false,
-        toolbar: 'undo redo | bold italic | link image| alignleft aligncenter alignright | code',
-        width: 900,
-        height: 300,
+        toolbar: 'undo redo | bold italic | link | alignleft aligncenter alignright | code',
         image_title: true,
         automatic_uploads: true,
         file_picker_types: 'image',
-        /* and here's our custom image picker*/
-        file_picker_callback: function (cb, value, meta) {
-            var input = document.createElement('input');
-            input.setAttribute('type', 'file');
-            input.setAttribute('accept', 'image/*');
-            /*
-            Note: In modern browsers input[type="file"] is functional without
-            even adding it to the DOM, but that might not be the case in some older
-            or quirky browsers like IE, so you might want to add it to the DOM
-            just in case, and visually hide it. And do not forget do remove it
-            once you do not need it anymore.
-            */
-            input.onchange = function () {
-                var file = this.files[0];
-                var reader = new FileReader();
-                reader.onload = function () {
-        /*
-        Note: Now we need to register the blob in TinyMCEs image blob
-        registry. In the next release this part hopefully won't be
-        necessary, as we are looking to handle it internally.
-        */
-                    var id = 'blobid' + (new Date()).getTime();
-                    var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-                    var base64 = reader.result.split(',')[1];
-                    var blobInfo = blobCache.create(id, file, base64);
-                    blobCache.add(blobInfo);
-                    /* call the callback and populate the Title field with the file name */
-                    cb(blobInfo.blobUri(), { title: file.name });
-                };
-                reader.readAsDataURL(file);
-            };
-            input.click();
+        setup: function (editor) {
+            editor.on('init', function (e) {
+                editor.setContent('{!!json_encode($content->desc) !!}');
+            });
         },
-        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
     });
 </script>
 @endpush
-@endsection
