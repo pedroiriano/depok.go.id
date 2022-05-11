e45bcd0783bfcc13a26f3412e4c17e981921e191
biobun <biobun@gmail.com>
2022-05-11 22:06:33 +0700

    Fix footer slider

--- resources/views/beranda-v1.blade.php
+++ resources/views/beranda-v1.blade.php
@@ -753,27 +753,6 @@
                         '</div>'
                     );
                 });
-                $(".owl-carousel").owlCarousel({
-                    loop:true,
-                    margin:10,
-                    nav:true,
-                    autoplay:true,
-                    autoplayTimeout:2000,
-                    autoplayHoverPause:true,
-                    dots: false,
-                    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-                    responsive:{
-                        0:{
-                            items:2
-                        },
-                        600:{
-                            items:3
-                        },
-                        1000:{
-                            items:5
-                        }
-                    }
-                });
             }
         });
         $.ajax({
@@ -967,8 +946,28 @@
 
             }
         });
-        
 
+        $(".owl-carousel").owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            autoplay:true,
+            autoplayTimeout:2000,
+            autoplayHoverPause:true,
+            dots: false,
+            navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+            responsive:{
+                0:{
+                    items:2
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
     });
 </script>
 @endpush
