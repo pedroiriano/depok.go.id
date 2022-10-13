d047f4e39e7f10c668b8500a10afcc3d22c8221b
Bio Abidzar <biobun@gmail.com>
2022-10-13 18:48:29 +0700

    change routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,20 @@
 <?php
 
+use App\Http\Controllers\AdministratorController;
+use App\Http\Controllers\AgendaController;
+use App\Http\Controllers\api\PendidikanController;
+use App\Http\Controllers\api\PendudukController;
+use App\Http\Controllers\BerandaController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DownloadsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InfografisController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\SliderController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,39 +29,39 @@
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', 'BerandaController@index')->name('beranda');
-Route::get('/agenda-kota', 'BerandaController@agenda')->name('list.agenda-kota');
-Route::get('/pengumuman', 'BerandaController@listPengumuman')->name('list.pengumuman');
-Route::get('/pengumuman/{url}', 'BerandaController@pengumuman')->name('pengumuman');
-Route::get('/infografis', 'BerandaController@infografis')->name('infografis');
-Route::get('/download/{file}', 'DownloadsController@download')->name('download');
-Route::get('/pimpinan-daerah', 'BerandaController@pimpinanDaerah')->name('pimpinan');
-Route::get('/demografi', 'api\PendudukController@index')->name('demografi');
-Route::get('/pendidikan', 'api\PendidikanController@index')->name('pendidikan');
+Route::get('/', [BerandaController::class, 'index'])->name('beranda');
+Route::get('/agenda-kota', [BerandaController::class, 'agenda'])->name('list.agenda-kota');
+Route::get('/pengumuman', [BerandaController::class, 'listPengumuman'])->name('list.pengumuman');
+Route::get('/pengumuman/{url}', [BerandaController::class, 'pengumuman'])->name('pengumuman');
+Route::get('/infografis', [BerandaController::class, 'infografis'])->name('infografis');
+Route::get('/download/{file}', [DownloadsController::class, 'download'])->name('download');
+Route::get('/pimpinan-daerah', [BerandaController::class, 'pimpinanDaerah'])->name('pimpinan');
+Route::get('/demografi', [PendudukController::class, 'index'])->name('demografi');
+Route::get('/pendidikan', [PendidikanController::class, 'index'])->name('pendidikan');
 
-Route::get('/get-kelurahan-list/{kecamatanId}', 'BerandaController@getKelurahan');
-Route::get('/layanan', 'BerandaController@layanan')->name('layanan');
+Route::get('/get-kelurahan-list/{kecamatanId}', [BerandaController::class, 'getKelurahan']);
+Route::get('/layanan', [BerandaController::class, 'layanan'])->name('layanan');
 
-Route::get('/api/agenda', 'BerandaController@AgendaAPI')->name('api.agenda');
-Route::get('/api/harga-komoditas', 'BerandaController@hargaKomoditasAPI')->name('api.harga-komoditas');
-Route::get('/api/berita', 'BerandaController@beritaAPI')->name('api.berita');
-Route::get('/api/cuacaBMKG', 'BerandaController@cuacaBMKGAPI');
-Route::get('/api/cuaca', 'BerandaController@cuacaAPI');
-Route::get('/api/youtube', 'BerandaController@youtubeAPI');
-Route::get('/api/kesehatan', 'BerandaController@kesehatanAPI');
-Route::get('/api/kependudukan', 'BerandaController@kependudukanAPI');
-Route::get('/api/kunjungan', 'BerandaController@kunjunganAPI');
-Route::get('/api/bphtb', 'BerandaController@bphtbAPI');
-Route::get('/api/pbb', 'BerandaController@pbbAPI');
-Route::get('/rss/berita', 'BerandaController@beritaRSS')->name('rss.berita');
-Route::get('/api/infografis', 'BerandaController@infografisAPI')->name('api.infografis');
-Route::get('/api/pengumuman', 'BerandaController@pengumumanAPI')->name('api.pengumuman');
-Route::get('/api/pendidikan', 'BerandaController@pendidikanAPI')->name('api.pendidikan');
-Route::get('/api/covid', 'BerandaController@covidAPI')->name('api.covid');
-Route::get('/api/demografi/{type}', 'api\PendudukController@demografi')
+Route::get('/api/agenda', [BerandaController::class,'AgendaAPI'])->name('api.agenda');
+Route::get('/api/harga-komoditas', [BerandaController::class,'hargaKomoditasAPI'])->name('api.harga-komoditas');
+Route::get('/api/berita', [BerandaController::class,'beritaAPI'])->name('api.berita');
+Route::get('/api/cuacaBMKG', [BerandaController::class,'cuacaBMKGAPI']);
+Route::get('/api/cuaca', [BerandaController::class,'cuacaAPI']);
+Route::get('/api/youtube', [BerandaController::class,'youtubeAPI']);
+Route::get('/api/kesehatan', [BerandaController::class,'kesehatanAPI']);
+Route::get('/api/kependudukan', [BerandaController::class,'kependudukanAPI']);
+Route::get('/api/kunjungan', [BerandaController::class,'kunjunganAPI']);
+Route::get('/api/bphtb', [BerandaController::class,'bphtbAPI']);
+Route::get('/api/pbb', [BerandaController::class,'pbbAPI']);
+Route::get('/rss/berita', [BerandaController::class,'beritaRSS'])->name('rss.berita');
+Route::get('/api/infografis', [BerandaController::class,'infografisAPI'])->name('api.infografis');
+Route::get('/api/pengumuman', [BerandaController::class,'pengumumanAPI'])->name('api.pengumuman');
+Route::get('/api/pendidikan', [BerandaController::class,'pendidikanAPI'])->name('api.pendidikan');
+Route::get('/api/covid', [BerandaController::class,'covidAPI'])->name('api.covid');
+Route::get('/api/demografi/{type}', [PendudukController::class, 'demografi'])
     ->name('api.demografi')
     ->where('type', 'Agama|Jenis Kelamin|Kelompok Umur|Pekerjaan|Pendidikan|Status Kawin');
-Route::get('/api/pendidikan/{type}', 'api\PendidikanController@pendidikan')
+Route::get('/api/pendidikan/{type}', [PendidikanController::class, 'pendidikan'])
     ->name('api.pendidikan')
     ->where('type', 'Sekolah|SekolahAkreditasi|SekolahKepemilikan|Pekerjaan|Pendidikan|SekolahKebutuhanKhusus');
 
@@ -58,30 +73,30 @@ Auth::routes([
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/home', [HomeController::class,'index'])->name('home');
     Route::group(['middleware' => ['permission:mengelola agenda']], function(){
-        Route::resource('/admin-agenda', 'AgendaController');
+        Route::resource('/admin-agenda', AgendaController::class);
     });
     Route::group(['middleware' => ['permission:mengelola dsw']], function () {
-        Route::resource('/admin-service', 'ServiceController');
-        Route::resource('/admin-kategori-dsw', 'CategoryController');
+        Route::resource('/admin-service', ServiceController::class);
+        Route::resource('/admin-kategori-dsw', CategoryController::class);
     });
     Route::group(['middleware' => ['permission:mengelola pengumuman']], function () {
-        Route::resource('/slider', 'SliderController');
+        Route::resource('/slider', SliderController::class);
     });
     Route::group(['middleware' => ['permission:mengelola infografis']], function () {
-        Route::resource('/admin-infografis', 'InfografisController', ['parameters' => ['admin-infografis' => 'infografis']]);
+        Route::resource('/admin-infografis', InfografisController::class, ['parameters' => ['admin-infografis' => 'infografis']]);
     });
     Route::group(['middleware' => ['permission:mengelola konten']], function () {
-        Route::get('/admin/content/{content}', 'AdministratorController@content')->name('admin.content');
-        Route::post('/admin/content/update/{content}', 'AdministratorController@updateContent')->name('admin.update.content');
+        Route::get('/admin/content/{content}', [AdministratorController::class, 'content'])->name('admin.content');
+        Route::post('/admin/content/update/{content}', [AdministratorController::class, 'updateContent'])->name('admin.update.content');
     });
     Route::group(['middleware' => ['role:administrator']], function () {
-        Route::resource('/user', 'UserController');
-        Route::post('/user/{id}/update-password', 'UserController@updatePassword')->name('user.update-password');
-        Route::get('/admin', 'AdministratorController@create')->name('admin.create');
-        Route::post('/admin', 'AdministratorController@store')->name('admin.store');
+        Route::resource('/user', UserController::class);
+        Route::post('/user/{id}/update-password', [UserController::class, 'updatePassword'])->name('user.update-password');
+        Route::get('/admin', [AdministratorController::class, 'create'])->name('admin.create');
+        Route::post('/admin', [AdministratorController::class, 'store'])->name('admin.store');
     });
 });
 
-Route::get('/{content}', 'BerandaController@content')->name('content');
+Route::get('/{content}', [BerandaController::class, 'content'])->name('content');
