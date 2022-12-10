fbc4523186a01840fa2e65c61bfec473aa87e931
ipiyuza <pdzeus83aw@gmail.com>
2022-12-10 11:32:36 +0700

    Route Fix

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
 Route::get('/', [BerandaController::class, 'index'])->name('beranda');
 Route::get('/agenda-kota', [BerandaController::class, 'agenda'])->name('list.agenda-kota');
 Route::get('/pengumuman', [BerandaController::class, 'listPengumuman'])->name('list.pengumuman');
