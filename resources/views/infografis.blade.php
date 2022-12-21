a6321f67500aa076ace06cc5c773977b26836804
Pedro Iriano <pdzeus83aw@gmail.com>
2022-12-22 00:14:11 +0700

    Fix Infographic Lightbox

--- resources/views/infografis.blade.php
+++ resources/views/infografis.blade.php
@@ -33,17 +33,16 @@
             @if (!empty($infografis))
                 @foreach ($infografis as $infografi)
                 <div class="group relative block overflow-hidden rounded-md transition-all duration-500">
-                    <img src="{{ asset('storage/uploads/infografis/'.$infografi->imageName) }}" class="group-hover:origin-center group-hover:scale-110 group-hover:rotate-3 transition duration-500" alt="Gambar Infografis">
-                    <div class="absolute inset-0 group-hover:bg-dark opacity-50 transition duration-500 z-0"></div>
-                    <div class="content">
-                        <div class="icon absolute z-10 hidden group-hover:block top-4 right-4 transition-all duration-500">
-                            <a href="{{ asset('storage/uploads/infografis/'.$infografi->imageName) }}" class="btn bg-indigo-600 hover:bg-indigo-700 border-indigo-600 hover:border-indigo-700 text-white btn-icon rounded-full lightbox"><i class="uil uil-camera"></i></a>
+                    <a href="{{ asset('storage/uploads/infografis/'.$infografi->imageName) }}" class="lightbox">
+                        <img src="{{ asset('storage/uploads/infografis/'.$infografi->imageName) }}" class="group-hover:origin-center group-hover:scale-110 group-hover:rotate-3 transition duration-500" alt="Gambar Infografis">
+                        <div class="absolute inset-0 group-hover:bg-dark opacity-50 transition duration-500 z-0"></div>
+                        <div class="content">
+                            <div class="title absolute z-10 hidden group-hover:block bottom-4 left-4">
+                                <div class="h6 text-md font-medium text-white hover:text-indigo-600 duration-500 ease-in-out">{{ $infografi->nama }}</div>
+                                <p class="text-slate-300 text-sm tag mb-0">Sumber: {{ $infografi->opd->nama }}</p>
+                            </div>
                         </div>
-                        <div class="title absolute z-10 hidden group-hover:block bottom-4 left-4">
-                            <div class="h6 text-md font-medium text-white hover:text-indigo-600 duration-500 ease-in-out">{{ $infografi->nama }}</div>
-                            <p class="text-slate-300 text-sm tag mb-0">Sumber: {{ $infografi->opd->nama }}</p>
-                        </div>
-                    </div>
+                    </a>
                 </div>
                 @endforeach
             @else
@@ -51,40 +50,6 @@
             @endif
         </div>
     </div>
-
-
-<!-- Modal toggle -->
-<button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="staticModal">
-    Toggle modal
-</button>
-
-<!-- Main modal -->
-<div id="staticModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="fixed top-16 left-0 right-0 z-999 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-    <div class="relative w-full h-full max-w-2xl md:h-auto">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- Modal header -->
-            <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                    Infografis
-                </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="staticModal">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
-                </button>
-            </div>
-            <!-- Modal body -->
-            <div class="flex p-6 space-y-6 items-center justify-center">
-                <div class="group relative block overflow-hidden rounded-md transition-all duration-500">
-                    <img src="assets/images/portfolio/1.jpg" class="group-hover:origin-center group-hover:scale-110 group-hover:rotate-3 transition duration-500 max-w-xs" alt="Gambar Infografis">
-                </div>
-            </div>
-            <!-- Modal footer -->
-            <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                <p class="text-slate-300 text-sm tag mb-0">Sumber: Pemerintah Kota Depok</p>
-            </div>
-        </div>
-    </div>
-</div>
 </section>
 <!-- END::Infographic -->
 @endsection
