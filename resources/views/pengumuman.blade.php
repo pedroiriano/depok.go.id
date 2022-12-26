a3c86a3e1cba835e181724297e0278eb97c2ad3c
Pedro Iriano <pdzeus83aw@gmail.com>
2022-12-27 00:05:51 +0700

    Add Detail Announcements Page

--- resources/views/pengumuman.blade.php
+++ resources/views/pengumuman.blade.php
@@ -1,61 +1,73 @@
-@push('css')
-<link rel="stylesheet" href={{ asset("css/owl/owl.carousel.min.css") }}>
-<link rel="stylesheet" href={{ asset("css/owl/owl.theme.default.min.css") }}>
-@endpush
-@push('js')
-<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-<script type="text/javascript">
-    $("#owl-carousel").owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        dots: false,
-        navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-        responsive:{
-            0:{
-                items:2
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
-</script>
-@endpush
-@extends('includes.layout')
+@extends('layouts.frontend')
+
 @section('content')
-<div class="container-fluid py-5">
+<!-- BEGIN::Header -->
+<section class="relative table w-full py-36 lg:py-44 bg-top bg-no-repeat bg-fixed" style="background-image: url({{ asset('img/content/layanan.png') }});">
+    <div class="absolute inset-0 bg-black opacity-75"></div>
+    <div class="container">
+        <div class="grid grid-cols-1 pb-8 text-center mt-10">
+            <h3 class="mb-6 md:text-4xl text-3xl md:leading-normal leading-normal font-medium text-white">Detail Pengumuman</h3>
+            <p class="text-slate-300 text-lg max-w-xl mx-auto">Dapatkan Informasi serta Layanan Publik terlengkap dan terkini seputar Kota Depok.</p>
+        </div>
+    </div>
+    <div class="absolute text-center z-10 bottom-5 right-0 left-0 mx-3">
+        <ul class="breadcrumb tracking-[0.5px] breadcrumb-light mb-0 inline-block">
+            <li class="inline breadcrumb-item uppercase text-[13px] font-bold duration-500 ease-in-out text-white/50 hover:text-white"><a href="/">Depok</a></li>
+            <li class="inline breadcrumb-item uppercase text-[13px] font-bold duration-500 ease-in-out text-white" aria-current="page"><a href="/pengumuman">Pengumuman</a></li>
+            <li class="inline breadcrumb-item uppercase text-[13px] font-bold duration-500 ease-in-out text-white" aria-current="page">Detail Pengumuman</li>
+        </ul>
+    </div>
+</section>
+<div class="relative">
+    <div class="shape absolute right-0 sm:-bottom-px -bottom-[2px] left-0 overflow-hidden z-1 text-white dark:text-slate-900">
+        <svg class="w-full h-auto" viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
+        </svg>
+    </div>
+</div>
+<!-- END::Header -->
+
+<!-- BEGIN::Announcements Detail -->
+<section class="relative md:py-24 py-16">
    <div class="container">
-      <div class="row justify-content-center">
-         <div class="col-md-8 col-sm-8">
-            <h1 class="display-4">{{ $pengumuman->nama }}</h1>
-            <h5 class="h6 text-muted pb-4">Sumber: {{ $pengumuman->opd->nama }}</h5>
-         </div>
-         <div class="col-md-10 col-sm-12 text-center">
-            <img src="{{ asset('storage/uploads/sliders/'.$pengumuman->imageName) }}" alt="" class="img-fluid rounded">
-         </div>
-         <div class="col-md-8 col-sm-12 pt-5">
-            <p class="lead" id="#deskripsi" style="font-size:1.2rem">{!! $pengumuman->deskripsi !!}</p>
-         </div>
-         @if($pengumuman->file)
-         <div class="col-5">
-            <a class="btn btn-success btn-sm shadow" href="{{ route('download', $pengumuman->file) }}">
-               <div class="row no-gutters text-light">
-                  <div class="col-3 d-flex justify-content-center align-items-center">
-                     <span><i class="fas fa-file-download fa-3x"></i></span>
-                  </div>
-                  <div class="col-9 text-left p-2">
-                     <h6 class="h6 font-weight-bold">Unduh</h6>
-                     <small class="text-light">{{ $pengumuman->file }}</small>
+      <div class="grid md:grid-cols-12 grid-cols-1 mt-8 gap-[30px]">
+         <div class="lg:col-span-8 md:col-span-6">
+            <div class="sticky top-20">
+               <div class="grid grid-cols-1 gap-[30px]">
+                  <div class="blog relative rounded-md shadow dark:shadow-gray-800 overflow-hidden">
+                     <div class="relative">
+                           <div class="relative md:shrink-0">
+                              <img class="h-full w-full object-cover" src="{{ asset('storage/uploads/sliders/'.$pengumuman->imageName) }}" alt="Gambar Pengumuman">
+                           </div>
+                     </div>
                   </div>
                </div>
-            </a>
+            </div>
          </div>
-         @endif
+         <div class="lg:col-span-4 md:col-span-6">
+            <h5 class="text-lg font-semibold bg-gray-50 dark:bg-slate-800 shadow dark:shadow-gray-800 rounded-md p-2 text-center">{{ $pengumuman->nama }}</h5>
+            <div class="flex items-center mt-8">
+                  <img src="assets/images/blog/06.jpg" class="h-16 rounded-md shadow dark:shadow-gray-800" alt="">
+                  <div class="ml-3">
+                     <div class="font-semibold hover:text-indigo-600">Sumber:</div>
+                     <p class="text-sm text-slate-400">{{ $pengumuman->opd->nama }}</p>
+                  </div>
+            </div>
+            <div class="flex items-center mt-4">
+                  <img src="assets/images/blog/07.jpg" class="h-16 rounded-md shadow dark:shadow-gray-800" alt="">
+                  <div class="ml-3">
+                     <div class="font-semibold hover:text-indigo-600">{!! $pengumuman->deskripsi !!}</div>
+                     <p class="text-sm text-slate-400">{{ $pengumuman->created_at->format('d-m-Y') }}</p>
+                  </div>
+            </div>
+            @if($pengumuman->file)
+               <div class="flex justify-center mt-8">
+                  <a href="{{ route('download', $pengumuman->file) }}" class="btn btn-link text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out">Unduh <i class="uil uil-arrow-down align-middle"></i></a>
+               </div>
+            @endif
+         </div>
       </div>
    </div>
-</div>
+</section>
+<!-- END::Announcements Detail -->
 @endsection
