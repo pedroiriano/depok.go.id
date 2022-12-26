039a6b951a31e30754850d0370b2d9a386e1768d
Pedro Iriano <pdzeus83aw@gmail.com>
2022-12-26 10:43:29 +0700

    Add Directives

--- app/Http/Policies/Extended.php
+++ app/Http/Policies/Extended.php
@@ -12,7 +12,7 @@ class Extended extends Basic
         parent::configure();
         
         $this
-            ->addDirective(Directive::STYLE, 'https://unicons.iconscout.com')
+            /* ->addDirective(Directive::STYLE, 'https://unicons.iconscout.com')
             ->addDirective(Directive::STYLE, 'https://cdnjs.cloudflare.com')
             ->addDirective(Directive::STYLE, 'https://unpkg.com')
             ->addDirective(Directive::STYLE, 'https://fonts.googleapis.com')
@@ -27,7 +27,16 @@ class Extended extends Basic
             ->addDirective(Directive::DEFAULT, 'https://fonts.gstatic.com')
             ->addDirective(Directive::DEFAULT, 'https://unicons.iconscout.com')
             ->addDirective(Directive::IMG, 'https://cms.depok.go.id')
-            ->addDirective(Directive::IMG, 'https://berita.depok.go.id');
+            ->addDirective(Directive::IMG, 'https://berita.depok.go.id') */
+            ->addDirective(Directive::BASE, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::CONNECT, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::DEFAULT, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::FORM_ACTION, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::IMG, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::MEDIA, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::OBJECT, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::SCRIPT, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com'])
+            ->addDirective(Directive::STYLE, ['https://unicons.iconscout.com', 'https://cdnjs.cloudflare.com', 'https://unpkg.com', 'https://fonts.googleapis.com', 'https://kit.fontawesome.com', 'https://cdn.jsdelivr.net', 'https://fontawesome.com', 'https://www.googletagmanager.com', 'https://ka-f.fontawesome.com', 'https://cms.depok.go.id', 'https://berita.depok.go.id', 'https://fonts.gstatic.com']);
             
             
     }
