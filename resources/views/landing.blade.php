9e4d41f022fbcc93d99fd60d824988b664552a9b
Pedro Iriano <pdzeus83aw@gmail.com>
2022-12-27 23:26:52 +0700

    Add Depok Data Card

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -193,8 +193,82 @@
                     <a href="" class="text-xl font-semibold hover:text-indigo-600 duration-500 ease-in-out">Sekilas Depok</a>
                     <h4 class="text-5xl mt-4" style="color: #1D4F88"><i class="pr-2" id="icon-weather-0"></i><span id="suhuHariIni"></span></h4>
                     <h6 class="text-lg" id="desc-weather">...</h6>
-                    <div class="mt-5">
-                        <a href="" class="btn btn-link text-indigo-600 hover:text-indigo-600 after:bg-indigo-600 duration-500 ease-in-out">Selengkapnya <i class="uil uil-arrow-right"></i></a>
+                    <div id="accordion-collapse" data-accordion="collapse" class="mt-4">
+                        <div class="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden">
+                            <h2 class="text-base font-semibold bg-white dark:bg-slate-900" id="accordion-collapse-heading-1">
+                                <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                                    <span class="text-sm">Penyakit Terpantau Bulan Ini</span>
+                                    <svg data-accordion-icon class="w-4 h-4 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </h2>
+                            <div id="accordion-collapse-body-1" class="hidden bg-white dark:bg-slate-900" aria-labelledby="accordion-collapse-heading-1">
+                                <div class="p-5">
+                                    <p class="text-slate-400 dark:text-gray-400 text-sm">Penyakit Terpantau Bulan Ini</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden mt-4">
+                            <h2 class="text-base font-semibold bg-white dark:bg-slate-900" id="accordion-collapse-heading-2">
+                                <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
+                                    <span class="text-sm">RSUD dan Puskesmas</span>
+                                    <svg data-accordion-icon class="w-4 h-4 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </h2>
+                            <div id="accordion-collapse-body-2" class="hidden bg-white dark:bg-slate-900" aria-labelledby="accordion-collapse-heading-2">
+                                <div class="p-5">
+                                    <p class="text-slate-400 dark:text-gray-400 text-sm">RSUD dan Puskesmas</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden mt-4">
+                            <h2 class="text-base font-semibold bg-white dark:bg-slate-900" id="accordion-collapse-heading-3">
+                                <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left" data-accordion-target="#accordion-collapse-body-3" aria-expanded="false" aria-controls="accordion-collapse-body-3">
+                                    <span class="text-sm">Jumlah Penduduk</span>
+                                    <svg data-accordion-icon class="w-4 h-4 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </h2>
+                            <div id="accordion-collapse-body-3" class="hidden bg-white dark:bg-slate-900" aria-labelledby="accordion-collapse-heading-3">
+                                <div class="p-5">
+                                    <p class="text-slate-400 dark:text-gray-400 text-sm">Jumlah Penduduk</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden mt-4">
+                            <h2 class="text-base font-semibold bg-white dark:bg-slate-900" id="accordion-collapse-heading-4">
+                                <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left" data-accordion-target="#accordion-collapse-body-4" aria-expanded="false" aria-controls="accordion-collapse-body-4">
+                                    <span class="text-sm">Jumlah Sekolah</span>
+                                    <svg data-accordion-icon class="w-4 h-4 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </h2>
+                            <div id="accordion-collapse-body-4" class="hidden bg-white dark:bg-slate-900" aria-labelledby="accordion-collapse-heading-4">
+                                <div class="p-5">
+                                    <p class="text-slate-400 dark:text-gray-400 text-sm">Jumlah Sekolah</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="relative shadow dark:shadow-gray-800 rounded-md overflow-hidden mt-4">
+                            <h2 class="text-base font-semibold bg-white dark:bg-slate-900" id="accordion-collapse-heading-5">
+                                <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left" data-accordion-target="#accordion-collapse-body-5" aria-expanded="false" aria-controls="accordion-collapse-body-5">
+                                    <span class="text-sm">Penerimaan Bulan Ini</span>
+                                    <svg data-accordion-icon class="w-4 h-4 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                                    </svg>
+                                </button>
+                            </h2>
+                            <div id="accordion-collapse-body-5" class="hidden bg-white dark:bg-slate-900" aria-labelledby="accordion-collapse-heading-5">
+                                <div class="p-5">
+                                    <p id="bphtb" class="text-slate-400 dark:text-gray-400 text-sm"></p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
