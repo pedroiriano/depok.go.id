4e8ee73ad31254f61bd3c081c111f6426c02ad5e
Pedro Iriano <pdzeus83aw@gmail.com>
2023-02-21 15:00:04 +0700

    Open Cors Comment

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -20,12 +20,12 @@ class SecurityHeaders
             $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
             $response->headers->set('Referrer-Policy', 'same-origin');
             $response->headers->set('Feature-Policy', "geolocation 'self'; vibrate 'none'");
-            /* $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' *.iconscout.com *.cloudflare.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' *.iconscout.com *.cloudflare.com 'unsafe-inline'; img-src 'self' * data:; font-src 'self' data: ; connect-src 'self' plausible.io/api/event; media-src 'self'; frame-src 'self' platform.twitter.com github.com *.youtube.com; object-src 'none'; base-uri 'self';");
+            /* $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' *.iconscout.com *.cloudflare.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' *.iconscout.com *.cloudflare.com 'unsafe-inline'; img-src 'self' * data:; font-src 'self' data: ; connect-src 'self' plausible.io/api/event; media-src 'self'; frame-src 'self' platform.twitter.com github.com *.youtube.com; object-src 'none'; base-uri 'self';"); */
             $response->headers->set('Expect-CT', 'enforce, max-age=30');
             $response->headers->set('Permissions-Policy', 'autoplay=(self), camera=(), encrypted-media=(self), fullscreen=(), geolocation=(self), gyroscope=(self), magnetometer=(), microphone=(), midi=(), payment=(), sync-xhr=(self), usb=()');
             $response->headers->set('Access-Control-Allow-Origin', '*');
             $response->headers->set('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token'); */
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Requested-With,X-CSRF-Token');
 
             $this->removeUnwantedHeaders($this->unwantedHeaders);
         }
